03fc45d41ef93dfe5ecbd80a50575cf092174206
Daniel Amaral Ribeiro <danielamaral567@gmail.com>
2023-11-18 19:53:01 -0300

    Video 02
    Adição da chamada de telas realizada no vídeo 02 do app NotIFica.

---
+++ appNotifica/Coordinator/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  appNotifica
+//
+//  Created by Daniel Amaral on 18/11/23.
+//
+
+import Foundation
+
+protocol Coordinator{
+    
+    func start()
+    
+}
---
+++ appNotifica/Coordinator/LoginCoordinator.swift
@@ -0,0 +1,25 @@
+//
+//  LoginCoordinator.swift
+//  appNotifica
+//
+//  Created by Daniel Amaral on 18/11/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginCoordinator: Coordinator{
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewController = LoginViewController()
+        
+        self.navigationController.pushViewController(viewController, animated: true)
+    }
+    
+}
