59b0092a4b6b2182568ce31f984badc0c523f6bc
Daniel Amaral Ribeiro <danielamaral567@gmail.com>
2023-11-27 10:52:12 -0300

    Criação dos arquivos para a tela Home
    Criação de arquivos MVVM para povoar a tela Home

---
+++ appNotifica/Source/Controller/NovaOcorrencia/NovaOcorrenciaView.swift
@@ -0,0 +1,8 @@
+//
+//  NovaOcorrenciaView.swift
+//  appNotifica
+//
+//  Created by IFB-BIOTIC-17 on 27/11/23.
+//
+
+import Foundation
---
+++ appNotifica/Source/Controller/NovaOcorrencia/NovaOcorrenciaViewController.swift
@@ -0,0 +1,8 @@
+//
+//  NovaOcorrenciaViewController.swift
+//  appNotifica
+//
+//  Created by IFB-BIOTIC-17 on 27/11/23.
+//
+
+import Foundation
